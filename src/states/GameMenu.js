28342532036ff91df6aeecb3ef8ab393116a37c5
Dylan Lang <dylanlang999@gmail.com>
2017-02-26 15:15:06 -0800

    removal of credits

--- src/states/GameMenu.js
+++ src/states/GameMenu.js
@@ -37,9 +37,6 @@ GameMenu.prototype = {
     this.addMenuOption('Options', function () {
       game.state.start("Options");
     });
-    this.addMenuOption('Credits', function () {
-      game.state.start("Credits");
-    });
   }
 };
 
